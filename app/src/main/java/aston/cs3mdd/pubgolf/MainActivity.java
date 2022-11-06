a84ce135380846cc584c990836dfa50cf3dba89b
Chris <chubby1226@gmail.com>
2022-11-06 19:24:46 +0000

    Updated scorecard

--- app/src/main/java/aston/cs3mdd/pubgolf/MainActivity.java
+++ app/src/main/java/aston/cs3mdd/pubgolf/MainActivity.java
@@ -1,6 +1,12 @@
 package aston.cs3mdd.pubgolf;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +21,10 @@ import aston.cs3mdd.pubgolf.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    EditText drink1, par1;
+    Button button;
+    SharedPreferences sp;
+    String drink1Str, par1Str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +42,27 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        drink1 = findViewById(R.id.drink1);
+        par1 = findViewById(R.id.par1);
+        button = findViewById(R.id.button);
+
+        sp = getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drink1Str = drink1.getText().toString();
+                par1Str = par1.getText().toString();
+
+                SharedPreferences.Editor editor = sp.edit();
+
+                editor.putString("drink", drink1Str);
+                editor.putString("par1", par1Str);
+                editor.commit();
+                Toast.makeText(MainActivity.this, "Information Saved.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
