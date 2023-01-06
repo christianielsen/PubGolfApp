8658781d5fb7724a1578c933bf3222cbd90f9fca
Chris <chubby1226@gmail.com>
2023-01-06 21:03:24 +0000

    Update to viewpager2

--- app/src/main/java/aston/cs3mdd/pubgolf/ui/map/MapFragment.java
+++ app/src/main/java/aston/cs3mdd/pubgolf/ui/map/MapFragment.java
@@ -23,6 +23,12 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -41,6 +47,7 @@ import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.material.tabs.TabLayout;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -57,315 +64,61 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
-    public static final String TAG = "AJB";
-    private FusedLocationProviderClient fusedLocationClient;
-    private Location mCurrentLocation;
-
-    private boolean requestingLocationUpdates;
-    private LocationRequest locationRequest;
-    private LocationCallback locationCallback;
+public class MapFragment extends Fragment {
 
     private FragmentMapBinding binding;
-    private GoogleMap mMap;
 
-    private Button btLocation, btRestaurant;
-    private TextView tvLatitude, tvLongitude;
-
-    ArrayList<HashMap<String, String>> restaurantList;
-    private ListView lv;
-    
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        SupportMapFragment supportMapFragment = (SupportMapFragment)
-//                getChildFragmentManager().findFragmentById(R.id.google_map);
-//
-//        supportMapFragment.getMapAsync(this);
-
-        restaurantList = new ArrayList<>();
-        lv = root.findViewById(R.id.listview);
-
-
-        String apiKey = getActivity().getResources().getString(R.string.API_KEY);
-        Places.initialize(getActivity().getApplicationContext(), apiKey);
-
-        // Initialize the AutocompleteSupportFragment.
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
-                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
-        // Specify the types of place data to return.
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ADDRESS, Place.Field.NAME, Place.Field.LAT_LNG));
-
-        // Set up a PlaceSelectionListener to handle the response.
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(@NotNull Place place) {
-                mMap.addMarker(new MarkerOptions().position(place.getLatLng()));
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 15));
-
-            }
-
-            @Override
-            public void onError(@NotNull Status status) {
-            }
-        });
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.i(TAG, "Location permissions have not been granted");
-            ActivityResultLauncher<String[]> locationPermissionRequest =
-                    registerForActivityResult(new ActivityResultContracts
-                                    .RequestMultiplePermissions(), result -> {
-                                Boolean fineLocationGranted = null;
-                                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                                    fineLocationGranted = result.getOrDefault(
-                                            Manifest.permission.ACCESS_FINE_LOCATION, false);
-                                } else {
-                                    fineLocationGranted =
-                                            result.get(Manifest.permission.ACCESS_FINE_LOCATION) != null ?
-                                                    result.get(Manifest.permission.ACCESS_FINE_LOCATION) :
-                                                    false;
-                                }
-                                Boolean coarseLocationGranted = null;
-                                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                                    coarseLocationGranted = result.getOrDefault(
-                                            Manifest.permission.ACCESS_COARSE_LOCATION, false);
-                                } else {
-                                    coarseLocationGranted =
-                                            result.get(Manifest.permission.ACCESS_COARSE_LOCATION) != null ?
-                                                    result.get(Manifest.permission.ACCESS_COARSE_LOCATION) :
-                                                    false;
-                                }
-                                if (fineLocationGranted != null && fineLocationGranted) {
-                                    Log.i(TAG, "Precise location access granted.");
-                                    this.getLastLocation();
-                                } else if (coarseLocationGranted != null && coarseLocationGranted) {
-                                    Log.i(TAG, "Only approximate location access granted.");
-                                    this.getLastLocation();
-                                } else {
-                                    Log.i(TAG, "No location access granted.");
-                                }
-                            }
-                    );
-// Before you perform the actual permission request, check whether your app
-// already has the permissions, and whether your app needs to show a permission
-// rationale dialog. For more details, see Request permissions.
-            locationPermissionRequest.launch(new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-            });
-        } else {
-            Log.i(TAG, "Location permissions already granted.");
-            getLastLocation();
-        }
-
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    Log.i(TAG, "Location Update: NO LOCATION");
-                    return;
-                }
-                for (Location location : locationResult.getLocations()) {
-                    // Update UI with location data
-                    Log.i(TAG, "Location Update: (" + location.getLatitude() +
-                            ", " + location.getLongitude() + ")");
-                    mCurrentLocation = location;
-                    updateUILocation(location);
-                }
-            }
-        };
-
-        btLocation = root.findViewById(R.id.btLocation);
-        tvLatitude = root.findViewById(R.id.tvLatitude);
-        tvLongitude = root.findViewById(R.id.tvLongitude);
-
-        btLocation.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                if (requestingLocationUpdates == false) {
-                    // starting
-                    requestingLocationUpdates = true;
-                    btLocation.setText("Stop Location Updates");
-                    startLocationUpdates();
-                    Log.i(TAG, "Updates started");
-                    double lat = mCurrentLocation.getLatitude();
-                    double lng = mCurrentLocation.getLongitude();
-                    LatLng latlng = new LatLng(lat, lng);
-                    mMap.clear();
-                    mMap.addMarker(new MarkerOptions().position(latlng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 15));
-
-                } else {
-                    // stopping
-                    requestingLocationUpdates = false;
-                    btLocation.setText("Start Location Updates");
-                    stopLocationUpdates();
-                    Log.i(TAG, "Updates stopped");
-                }
-            }
-        });
-
-
-        btRestaurant = root.findViewById(R.id.btRestaurant);
-        btRestaurant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Retrofit Builder
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://maps.googleapis.com/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-
-                //Instance for interface
-                APICall APICall = retrofit.create(APICall.class);
-
-                String lat = String.valueOf(mCurrentLocation.getLatitude());
-                String lng = String.valueOf(mCurrentLocation.getLongitude());
-                String loc = lat + "," + lng;
-                String radius = "5000";
-                String type = "restaurant";
-                String keyword = "bar,restaurant";
-                String key = getActivity().getResources().getString(R.string.API_KEY);
-
-                Call<ResultsItem> call = APICall.getData(loc, radius, type, keyword, key);
-                Log.i("AJB", call.toString());
-
-
-                call.enqueue(new Callback<ResultsItem>() {
-                    @Override
-                    public void onResponse(Call<ResultsItem> call, Response<ResultsItem> response) {
-                        //Checking for the response
-
-                        if (response.code() != 200) {
-                            Log.i(TAG, "API error");
-                            return;
-                        }
-
-                        if (response.isSuccessful()) {
-                            //Loop through results
-                            for (int i = 0; i < response.body().getResults().size(); i++) {
-                                //Get LatLng and place name of each result
-                                Double lat = response.body().getResults().get(i).getGeometry().getLocation().getLat();
-                                Double lng = response.body().getResults().get(i).getGeometry().getLocation().getLng();
-                                String rName = response.body().getResults().get(i).getName();
-                                String address = response.body().getResults().get(i).getVicinity();
-                                Log.i("AJB", lat.toString() + lng.toString());
-
-                                //Place markers with title
-                                LatLng rLocation = new LatLng(lat, lng);
-//                                mMap.addMarker(new MarkerOptions().position(rLocation).title(namey));
-//                                mMap.animateCamera(CameraUpdateFactory.newLatLng(rLocation));
-
-                                HashMap<String, String> restaurants = new HashMap<>();
-                                restaurants.put("name", rName);
-                                restaurants.put("address", address);
-
-                                restaurantList.add(restaurants);
-
-                                ListAdapter adapter = new SimpleAdapter(
-                                        getActivity(),
-                                        restaurantList,
-                                        R.layout.row_layout,
-                                        new String[]{"name", "address"},
-                                        new int[]{R.id.tView, R.id.tView2});
-                                lv.setAdapter(adapter);
-
-                            }
-                            Toast.makeText(getActivity(), "Found " + response.body().getResults().size() + " pubs", Toast.LENGTH_LONG).show();
-
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResultsItem> call, Throwable t) {
-                    }
-                });
-
-
-            }
-        });
-
         return root;
     }
 
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        MyTabPagerAdapter tabPager = new MyTabPagerAdapter(getChildFragmentManager());
+
+        ViewPager2 viewPager = getActivity().findViewById(R.id.view_pager);
+        viewPager.setAdapter(tabPager);
+
+        TabLayout tabLayout = getActivity().findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        stopLocationUpdates();
         binding = null;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.mMap = googleMap;
-    }
 
-    public void getLastLocation() {
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+    static class MyTabPagerAdapter extends FragmentStateAdapter {
+
+        public MyTabPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
         }
-        fusedLocationClient.getCurrentLocation(PRIORITY_BALANCED_POWER_ACCURACY, null)
-                //fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            //mCurrentLocation = location;
-                            updateUILocation(location);
-                            // Logic to handle location object
-                            Log.i(TAG, "We got a location: (" + location.getLatitude() +
-                                    ", " + location.getLongitude() + ")");
 
-                        } else {
-                            Log.i(TAG, "We failed to get a last location");
-                        }
-                    }
-                });
-
-    }
-
-    private void startLocationUpdates() {
-        if (locationRequest == null) {
-            createLocationRequest();
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch(position) {
+                case 0:
+                    return new MapTab();
+                case 1:
+                    return new RestaurantTab();
+                default:
+                    return new MapTab();
+            }
         }
-        fusedLocationClient.requestLocationUpdates(locationRequest,
-                locationCallback,
-                Looper.getMainLooper());
+
+        @Override
+        public int getItemCount() {
+            return 2;
+        }
     }
-
-    protected void createLocationRequest() {
-        locationRequest = LocationRequest.create();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
-
-    private void stopLocationUpdates() {
-        Log.i(TAG, "Pause: stopping location updates");
-        fusedLocationClient.removeLocationUpdates(locationCallback);
-    }
-
-    private void updateUILocation(Location location) {
-        this.mCurrentLocation = location;
-        tvLatitude.setText("Lat: " + location.getLatitude());
-        tvLongitude.setText("Lon:" + location.getLongitude());
-        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
-
-    }
-
 }
--- app/src/main/java/aston/cs3mdd/pubgolf/ui/map/RestaurantTab.java
+++ app/src/main/java/aston/cs3mdd/pubgolf/ui/map/RestaurantTab.java
@@ -8,14 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import aston.cs3mdd.pubgolf.R;
+import aston.cs3mdd.pubgolf.databinding.FragmentRestaurantBinding;
 
 public class RestaurantTab extends Fragment {
+
+    private FragmentRestaurantBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant, container, false);
+        binding = FragmentRestaurantBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
     }
+
 }
